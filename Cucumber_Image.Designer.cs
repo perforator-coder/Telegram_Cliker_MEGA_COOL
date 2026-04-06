42d100994bf6b4a4f25a73b6a4c13dbcc53c633d
perforator-coder <nicita.paschkovsky@mail.ru>
2026-04-06 17:08:40 +0300

    hex_fix

--- Cucumber_Image.Designer.cs
+++ Cucumber_Image.Designer.cs
@@ -43,7 +43,7 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(260, 211);
+            this.pictureBox1.Size = new System.Drawing.Size(238, 214);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -54,7 +54,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(258, 210);
+            this.ClientSize = new System.Drawing.Size(236, 213);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
